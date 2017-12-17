2e63584f5d5901bc8f243babbfee1ad73bf03e7f
Zhe Yu <zyu9@ncsu.edu>
2017-12-17 02:41:21 -0500

    all

--- Error/src/mar.py
+++ Error/src/mar.py
@@ -20,7 +20,7 @@ class MAR(object):
         self.syn_thres = 0.8
         self.enable_est = True
         self.interval = 50000000
-        self.er = 0.30
+        self.er = 0.00
 
 
     def create(self,filename):
