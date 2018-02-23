d852cbb160e7bdfc979c21b4c9e832361c676b8a
Zhe Yu <zyu9@ncsu.edu>
2018-02-23 14:48:04 -0500

    error

--- Error/src/run_hpc.sh
+++ Error/src/run_hpc.sh
@@ -1,7 +1,7 @@
 #! /bin/tcsh
 # chmod +x run_hpc.sh
-rm err/*
-rm out/*
+# rm err/*
+# rm out/*
 foreach VAR (`seq 0 1 30`)
   bsub -q standard -W 1500 -n 2 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J /share/tjmenzie/zyu9/miniconda2/bin/python2.7 runner.py error_hpcc $VAR
 end
